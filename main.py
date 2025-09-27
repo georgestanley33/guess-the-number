fe0c2a5c0ea5e8fbdc53c502a5c1475380df8bf0
George <georgestanley33@gmail.com>
2025-09-27 22:16:04 +0100

    Add higher or lower, and number to well done message

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ print(number) #For testing
 while True:
     choice = int(input("Guess a number between 0 and 100"))
     if choice == number:
-        print("Well done, you guessed the number!")
+        print("Well done, you guessed the number, it was " , number)
         break
+    else:
+        if choice < number:
+            print("Higher")
+        if choice > number:
+            print("Lower")
